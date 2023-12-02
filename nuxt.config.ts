8421786f35a7c057c65fcf94e9ffb61f29941160
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-12-02 19:59:09 +0500

    Added formkit and login page:

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: ["@formkit/nuxt"],
+  formkit: {
+    autoImport: true,
+  },
   css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
