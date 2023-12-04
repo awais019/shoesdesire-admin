a9d775c5305f04ee86d92a41beb1b76b18e6f8e3
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-12-04 11:04:24 +0500

    Added categories auto complete

---
+++ components/CategoriesAutoComplete.vue
@@ -0,0 +1,121 @@
+<script lang="ts" setup>
+  import type { Category } from "~/types/category";
+  import { ref, computed } from "vue";
+  import {
+    Combobox,
+    ComboboxInput,
+    ComboboxButton,
+    ComboboxOptions,
+    ComboboxOption,
+    TransitionRoot,
+  } from "@headlessui/vue";
+  import { CheckIcon, ChevronUpDownIcon } from "@heroicons/vue/20/solid";
+
+  const { data } = await useCategory().getAll();
+
+  const categories = data.value?.data || [];
+
+  let selected = ref(categories[0]);
+
+  const selectedCategories = ref<Category[]>([]);
+
+  let query = ref("");
+
+  let filteredPeople = computed(() =>
+    query.value === ""
+      ? categories
+      : categories.filter((category) =>
+          category.name
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.value.toLowerCase().replace(/\s+/g, ""))
+        )
+  );
+
+  const values = computed(() => {
+    return selectedCategories.value.reduce((acc, category, index) => {
+      if (index == selectedCategories.value.length - 1)
+        return acc + category.name;
+      return acc + category.name + ", ";
+    }, "");
+  });
+
+  watch(selected, () => {
+    selectedCategories.value.push(selected.value);
+  });
+</script>
+
+<template>
+  <div class="">
+    <Combobox v-model="selected">
+      <div class="relative mt-1">
+        <div
+          class="relative w-full cursor-default overflow-hidden rounded-lg bg-anti_flash_white text-left focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm"
+        >
+          <ComboboxInput
+            class="w-full border-none focus:ring-0 bg-anti_flash_white"
+            id="categories"
+            @change="query = $event.target.value"
+            :display-value="() => values || 'Select a category'"
+          >
+          </ComboboxInput>
+          <ComboboxButton
+            class="absolute inset-y-0 right-0 flex items-center pr-2"
+          >
+            <ChevronUpDownIcon
+              class="h-5 w-5 text-gray-400"
+              aria-hidden="true"
+            />
+          </ComboboxButton>
+        </div>
+        <TransitionRoot
+          leave="transition ease-in duration-100"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+          @after-leave="query = ''"
+        >
+          <ComboboxOptions
+            class="absolute mt-1 max-h-48 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm"
+          >
+            <div
+              v-if="filteredPeople.length === 0 && query !== ''"
+              class="relative cursor-default select-none px-4 py-2 text-gray-700"
+            >
+              Nothing found.
+            </div>
+
+            <ComboboxOption
+              v-for="person in filteredPeople"
+              as="template"
+              :key="person.id"
+              :value="person"
+              v-slot="{ selected, active }"
+            >
+              <li
+                class="relative cursor-default select-none py-2 pl-10 pr-4"
+                :class="{
+                  'bg-teal-600 text-white': active,
+                  'text-gray-900': !active,
+                }"
+              >
+                <span
+                  class="block truncate"
+                  :class="{ 'font-medium': selected, 'font-normal': !selected }"
+                >
+                  {{ person.name }}
+                </span>
+                <span
+                  v-if="selected"
+                  class="absolute inset-y-0 left-0 flex items-center pl-3"
+                  :class="{ 'text-white': active, 'text-teal-600': !active }"
+                >
+                  <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                </span>
+              </li>
+            </ComboboxOption>
+          </ComboboxOptions>
+        </TransitionRoot>
+      </div>
+    </Combobox>
+  </div>
+</template>
---
+++ components/ProductForm.vue
@@ -0,0 +1,124 @@
+<script setup lang="ts">
+  const { create } = useCategory();
+
+  const emits = defineEmits<{
+    (e: "close"): void;
+  }>();
+
+  async function handleSubmit(values: any) {
+    const body = new FormData();
+    for (const key in values) {
+      if (key === "image") {
+        body.append(key, values[key][0].file);
+      } else {
+        body.append(key, values[key]);
+      }
+    }
+
+    const { data } = create(body);
+
+    if (data.value) {
+      emits("close");
+    }
+  }
+</script>
+
+<template>
+  <FormKit
+    type="form"
+    form-class="p-6 bg-white rounded-md"
+    :actions="false"
+    message-class="text-red-400 text-sm"
+    @submit="handleSubmit"
+  >
+    <h1 class="text-2xl font-semibold">New Product</h1>
+    <span class="text-sm text-gainsboro">add a new product</span>
+    <div class="flex gap-4 mt-8 mb-4">
+      <FormKit
+        type="text"
+        name="name"
+        placeholder="Product Name"
+        label="Name"
+        input-class="bg-anti_flash_white rounded-md border-none"
+        label-class="block mb-2 font-medium"
+        message-class="text-red-400 text-sm"
+        validation="required|length:3"
+        :validation-messages="{
+          required: 'The name field is required',
+          length: 'The name must be at least 3 characters',
+        }"
+      />
+      <FormKit
+        type="text"
+        name="description"
+        placeholder="Product Description"
+        label="Description"
+        message-class="text-red-400 text-sm"
+        input-class="bg-anti_flash_white rounded-md border-none"
+        label-class="block mb-2 font-medium"
+        validation="required|length:3"
+        :validation-messages="{
+          required: 'The description field is required',
+          min: 'The description must be at least 3 characters',
+        }"
+      />
+      <FormKit
+        type="number"
+        name="price"
+        placeholder="Product price"
+        label="Price"
+        input-class="bg-anti_flash_white rounded-md border-none"
+        label-class="block mb-2 font-medium"
+        message-class="text-red-400 text-sm"
+        validation="required|min:10"
+        :validation-messages="{
+          required: 'The price field is required',
+          min: 'The price must be at least 10',
+        }"
+      />
+    </div>
+    <div class="flex gap-4 mt-8 mb-4">
+      <FormKit
+        type="number"
+        name="stock"
+        placeholder="Product Stock"
+        label="Stock"
+        message-class="text-red-400 text-sm"
+        input-class="bg-anti_flash_white rounded-md border-none"
+        label-class="block mb-2 font-medium"
+        validation="required|min:1"
+        :validation-messages="{
+          required: 'The stock field is required',
+          min: 'The stock must be at least 1',
+        }"
+      />
+      <div class="grow">
+        <label for="categories" class="block mb-2 font-medium"
+          >Categories</label
+        >
+        <CategoriesAutoComplete />
+      </div>
+    </div>
+    <FormKit
+      type="file"
+      name="image"
+      label="Image"
+      accept="image/x-png,image/gif,image/jpeg"
+      message-class="text-red-400 text-sm"
+      label-class="block mb-2 font-medium"
+      file-name-class="hidden"
+      file-remove-class="text-red-400"
+      validation="required"
+      :validation-messages="{
+        required: 'The image field is required',
+      }"
+    />
+    <FormKit
+      type="submit"
+      outer-class="mt-8 text-white bg-light_azure hover:bg-azure p-2 text-center mx-60 rounded-md"
+      input-class="font-bold text-xl"
+    />
+  </FormKit>
+</template>
+
+<style scoped></style>
